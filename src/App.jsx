088a8b175579ff718dff2a1b7bad46c6d2f39237
Vickkk <justusofnigeria@gmail.com>
2023-03-05 20:08:38 +0100

    First push

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ISO6391 from "iso-639-1";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="min-h-screen bg-primary flex items-center px-3 md:px-5">
+				<div className="translator-container container bg-white p-4 rounded-md">
+					<span>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, alias? Fugit tempora dolore inventore odit repudiandae
+						facere iusto non, pariatur nesciunt. Sed mollitia fugiat minima sunt quia est placeat officia.
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
