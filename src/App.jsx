87f0c1825a03adf865bc750cc082e948cb1b7a32
Vickkk <justusofnigeria@gmail.com>
2023-03-06 01:07:42 +0100

    Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import ISO6391 from "iso-639-1";
 import "./App.css";
+import { Icon } from "@iconify-icon/react";
 
 function App() {
 	return (
 		<div className="App">
 			<div className="min-h-screen bg-primary flex items-center px-3 md:px-5">
-				<div className="translator-container container bg-white p-4 rounded-md">
-					<span>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, alias? Fugit tempora dolore inventore odit repudiandae
-						facere iusto non, pariatur nesciunt. Sed mollitia fugiat minima sunt quia est placeat officia.
-					</span>
+				<div className=" translator-container container bg-white p-4 md:p-8 rounded-md">
+					<div className="pre-form-container ">
+						<form action="">
+							<div className="form-inner grid md:grid-cols-2 gap-8 md:gap-0 ">
+								<div className="from_lang flex flex-col">
+									<textarea
+										id="text-input"
+										rows="8"
+										className="resize-none block w-full md:rounded-none md:rounded-tl-md transition duration-300 ease-in-out border-b-0 text-sm text-gray-800 bg-white border"
+										placeholder="Enter text"
+										required></textarea>
+									<div className="border"></div>
+								</div>
+								<div className="to_lang flex flex-col">
+									<textarea
+										id="text-output"
+										rows="8"
+										className="resize-none block w-full md:rounded-none md:rounded-tr-md transition duration-300 ease-in-out border-b-0 text-sm text-gray-800 bg-white border"
+										placeholder="Translation"
+										required></textarea>
+									<div className=""></div>
+								</div>
+							</div>
+							<div className="submit-button text-center mt-4">
+								<button type="submit" className="py-4 bg-primary w-full rounded-lg text-white font-semibold">
+									Translate Text
+								</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: "#57B8FF",
+				primary: "#2F52E0",
 			},
 		},
 	},
