96ed6ba490d51abc25f18527efb922991a2caad8
Vickkk <justusofnigeria@gmail.com>
2023-03-12 18:15:20 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { Icon } from "@iconify-icon/react";
+import { createContext, useState } from "react";
+import Index from "./Index";
+import LanguageDetect from "./LanguageDetect";
+
+export const ErrorSuccesContainerContext = createContext();
+
+function App() {
+	const [view, changeView] = useState(false); //default view is translation
+	const [container, setContainer] = useState(false);
+	const [error, setError] = useState(null); //set error if failed
+	const [success, setSuccess] = useState(null); //set success if successful
+	const [loading, setLoading] = useState(false); //loading to get result of translation
+
+	return (
+		<ErrorSuccesContainerContext.Provider
+			value={{ view, changeView, container, setContainer, error, setError, success, setSuccess, loading, setLoading }}>
+			<div className="App">
+				<div className="p-4">
+					<button
+						className="transition duration-300 ease-in-out bg-white rounded-md p-3 text-xs leading-tight font-bold shadow-md hover:bg-primary border hover:shadow-none hover:text-white hover:bg-opacity-50 hover:border hover:border-white"
+						onClick={() => changeView((prev) => !prev)}>
+						{view ? "Detect language" : "Translate language"}
+					</button>
+				</div>
+				<div className="container-title text-center">
+					<h1 className="text-white text-2xl md:text-4xl font-semibold">{view ? "Language Translator 🚀" : "Detect Language"}</h1>
+					<small className="text-white">Enjoy translating as much as 183 languages😁💅</small>
+				</div>
+				<div className="relative">
+					<div
+						className={`flex flex-col top-2 right-2 z-10 fixed gap-1 transition duration-300 ease-in-out w-max${
+							container === true ? "opacity-100 pointer-events-auto visible" : "invisible opacity-0 pointer-events-none"
+						}`}>
+						<div
+							className={`transition duration-200 ease-in-out shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] bg-white rounded-md w-max sm:w-64 h-max py-3 px-3 items-center gap-2 md:whitespace-nowrap ${
+								success ? "flex" : "hidden"
+							}`}>
+							<Icon icon="fa-solid:check-circle" className="text-green-600" />
+							<small className="font-semibold text-xs">{success}</small>
+						</div>
+						<div
+							className={`transition duration-200 ease-in-out shadow-[rgba(99,99,99,0.2)_0px_2px_8px_0px] bg-white rounded-md w-max sm:w-64 h-max py-3 px-3 flex items-center gap-2 whitespace-nowrap  ${
+								error ? "flex" : "hidden"
+							}`}>
+							<Icon icon="material-symbols:cancel-rounded" className="text-red-500 scale-110" />
+							<small className="font-semibold text-xs">{error}</small>
+						</div>
+					</div>
+				</div>
+				{view ? <Index /> : <LanguageDetect />}
+			</div>
+		</ErrorSuccesContainerContext.Provider>
+	);
+}
+
+export default App;
---
+++ src/LanguageDetect.jsx
@@ -0,0 +1,84 @@
+import { Icon } from "@iconify-icon/react";
+import { useContext, useState } from "react";
+import { ErrorSuccesContainerContext } from "./App";
+import DetectLanguage from "detectlanguage";
+
+const API_KEY_TWO = import.meta.env.VITE_LANG_DETECT_KEY;
+const languageDetector = new DetectLanguage(`${API_KEY_TWO}`);
+
+const text = "ọlọrun iyanu";
+
+const LanguageDetect = () => {
+	const [loading, setLoading] = useState(false);
+
+	function runDetection(e) {
+		e.preventDefault();
+	}
+
+	const { view, changeView, container, setContainer, error, setError, success, setSuccess } = useContext(ErrorSuccesContainerContext);
+
+	return (
+		<div className="translator-container container col-span-full md:col-start-1 md:col-end-12 lg:col-start-2 lg:col-end-10 bg-white p-4 md:p-8 rounded-md mx-auto md:mt-0 mt-4 h-max">
+			<form action="" onSubmit={runDetection}>
+				<div>
+					<textarea
+						id="text-input"
+						rows="8"
+						spellCheck="false"
+						className="resize-none block w-full rounded-md transition duration-300 ease-in-out text-sm text-gray-800 bg-white border focus:border-primary focus:shadow-none focus:outline-0 px-3 py-2"
+						placeholder="Enter language text"></textarea>
+				</div>
+				<div className="grid grid-cols-4 py-3">
+					<div className="flex flex-col items-center">
+						<button
+							type="button"
+							className="p-2 rounded-full transition duration-300 ease-in-out bg-transparent hover:bg-gray-100 w-max mx-auto">
+							<Icon className="flex" icon="ph:speaker-high-light" />
+						</button>
+						<span className="leading-none text-xs font-semibold">Read</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<button
+							type="button"
+							className="p-2 rounded-full transition duration-300 ease-in-out bg-transparent hover:bg-gray-100 w-max mx-auto">
+							<Icon className="flex" icon="clarity:scissors-line" />
+						</button>
+						<span className="leading-none text-xs font-semibold">Read</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<button
+							type="button"
+							className="p-2 rounded-full transition duration-300 ease-in-out bg-transparent hover:bg-gray-100 w-max mx-auto">
+							<Icon className="flex" icon="fluent:clipboard-paste-24-regular" />
+						</button>
+						<span className="leading-none text-xs font-semibold">Read</span>
+					</div>
+					<div className="flex flex-col items-center">
+						<button
+							type="button"
+							className="p-2 rounded-full transition duration-300 ease-in-out bg-transparent hover:bg-gray-100 w-max mx-auto">
+							<Icon className="flex" icon="fluent:clipboard-24-regular" />
+						</button>
+						<span className="leading-none text-xs font-semibold">Read</span>
+					</div>
+				</div>
+				<div className="submit-button text-center">
+					<button
+						disabled={loading ? true : false}
+						type="submit"
+						className={`transition ease-in-out duration-300 py-3 justify-center bg-primary w-full rounded-lg flex items-center gap-4 min-h-[3rem] ${
+							loading ? "cursor-not-allowed" : "cursor-pointer"
+						} hover:bg-opacity-95`}>
+						<span className=" text-white font-semibold text-sm">Detect Language</span>
+						<div
+							className={`h-6 w-6 border-2 border-l-transparent rounded-full animate-spin ${
+								loading === false ? "hidden" : "inline"
+							}`}></div>
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default LanguageDetect;
